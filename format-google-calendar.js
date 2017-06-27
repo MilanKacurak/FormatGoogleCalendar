6bbd0c49cb9054558738676ff4ae54f1f8dab456
trentpiercy <mcpvpxie@gmail.com>
2017-06-27 00:22:34 -0500

    Format override

--- format-google-calendar.js
+++ format-google-calendar.js
@@ -399,7 +399,7 @@ var formatGoogleCalendar = (function() {
                 pastSelector: '#events-past',
                 upcomingHeading: '<h2>Upcoming events</h2>',
                 pastHeading: '<h2>Past events</h2>',
-                format: ['*date*', ': ', '*summary*', ' &mdash; ', '*description*', ' in ', '*location*']
+                format: ['*date*', ': ', '*summary*', ' &mdash; ', '*description*', ' in ', '*location*', '</br>', '*htmlLink*']
             };
 
             settings = mergeOptions(settings, settingsOverride);
